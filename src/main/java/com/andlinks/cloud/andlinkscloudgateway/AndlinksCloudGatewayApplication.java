fd28c239ff37dac72d2e9fd2a83fe9f2e258ef7f
chencong <1085576236@qq.com>
2018-07-09 11:12:04 +0800

    first commot

---
+++ src/main/java/com/andlinks/cloud/andlinkscloudgateway/AndlinksCloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.andlinks.cloud.andlinkscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndlinksCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndlinksCloudGatewayApplication.class, args);
+	}
+}
